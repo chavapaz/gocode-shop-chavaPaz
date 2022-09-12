b89834a4bf0350a9095e56470c3f37b2fbe4bdfe
chavapaz <chavipluz@gmial.com>
2022-09-12 16:01:10 +0300

    first commit

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import productDetail from "./ProductDetail.vue";
+
+export default defineComponent({
+  components: { productDetail },
+  data() {
+    return {};
+  },
+});
+</script>
+
+<template>
+
+  <product-detail/>
+  <product-detail/>
+  <product-detail/>
+  <product-detail/>
+</template>
